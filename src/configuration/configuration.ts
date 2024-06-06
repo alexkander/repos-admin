2475569d22b8c6a29f2263d0cad78161d5061ca2
Alexander Rondon <arondn2@gmail.com>
2024-06-06 07:28:42 -0500

    chore: add influxdb connection test

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -8,4 +8,5 @@ export const configuration = {
   REPOSITORIES_DIRECTORY: process.env.REPOSITORIES_DIRECTORY || '/repositories',
   ALLOW_DESTRUCTIVE_ACTIONS: process.env.ALLOW_DESTRUCTIVE_ACTIONS === 'true',
   SWAGGER_ACTIVE: process.env.SWAGGER_ACTIVE === 'true',
+  INFLUXDB_TOKEN: process.env.INFLUXDB_TOKEN,
 };
--- src/controllers/repo.controller.ts
+++ src/controllers/repo.controller.ts
@@ -1,10 +1,19 @@
+import { InfluxDB, Point } from '@influxdata/influxdb-client';
+import { InfluxDBClient } from '@influxdata/influxdb3-client';
 import { Controller, Get, Param, Post, Query, Render } from '@nestjs/common';
 import { Types } from 'mongoose';
+import { configuration } from 'src/configuration/configuration';
 import { Repo } from '../schemas/repo.schema';
 import { RepoService } from '../services/repo.service';
 import { SearchService } from '../services/search.service';
 import { TableQueryParams } from '../types/utils.types';
 import { repoSearchValidation } from '../validations/repo.search.validator';
+
+const token = configuration.INFLUXDB_TOKEN;
+const url = 'http://localhost:8086';
+const org = 'alex';
+const bucket = 'repo-admin';
+const client = new InfluxDB({ url, token });
 
 const fields = [
   { field: 'directory', text: 'directory' },
@@ -61,13 +70,70 @@ export class RepoController {
   }
 
   @Post('/:id/syncAll')
-  syncAllById(@Param('id') id: Types.ObjectId) {
-    return this.repoService.syncRepoById(id, {
+  async syncAllById(@Param('id') id: Types.ObjectId) {
+    const now = Date.now();
+    const result = await this.repoService.syncRepoById(id, {
       doFetch: true,
       syncBranches: true,
       syncRemotes: true,
       syncTags: true,
     });
+
+    const duration = Date.now() - now;
+    this.writePoint(duration);
+    return result;
+  }
+
+  @Get('/stats1')
+  async stats1() {
+    const queryClient = client.getQueryApi(org);
+    const fluxQuery = `from(bucket: "${bucket}")
+      |> range(start: -2h)
+      |> filter(fn: (r) => r._measurement == "measurement1")`;
+
+    const result = new Promise((resolve, reject) => {
+      const tableObject: any[] = [];
+      queryClient.queryRows(fluxQuery, {
+        next: (row, tableMeta) => {
+          tableObject.push(tableMeta.toObject(row));
+        },
+        error: reject,
+        complete: () => {
+          resolve(tableObject);
+        },
+      });
+    });
+    return await result;
+  }
+
+  @Get('/stats2')
+  async stats2() {
+    const client = new InfluxDBClient({ host: url, token });
+    const query = `SELECT * FROM 'repo-admin' WHERE time >= now() - INTERVAL '2h'`;
+    const result = await client.query(query, org);
+
+    const rows: any[] = [];
+
+    for await (const row of result) {
+      rows.push(row);
+    }
+    client.close();
+    return rows;
+  }
+
+  writePoint(duration: number) {
+    const writeClient = client.getWriteApi(org, bucket, 'ns');
+    const point = new Point('measurement1')
+      .tag('tagname1', 'tagvalue1')
+      .intField('field1', duration);
+
+    setTimeout(() => {
+      writeClient.writePoint(point);
+    }, 1000);
+
+    setTimeout(() => {
+      writeClient.flush();
+    }, 2000);
   }
 
   @Get('/:id/checkStatus')
